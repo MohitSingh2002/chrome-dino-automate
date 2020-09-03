f5a90d813577d2f486e9d09abc8d9a4aaaca50e0
Mohit <mohitsingh11082002@gmail.com>
2020-09-03 09:07:35 +0530

    Initial commit

---
+++ dino-automate.py
@@ -0,0 +1,29 @@
+import pyautogui
+from PIL import Image, ImageGrab
+import time
+
+
+def jump(key):
+    for i in range(300, 415):
+        for j in range(410, 563):
+            if key[i, j] < 100:
+                pyautogui.keyDown("down")
+                return
+
+    for i in range(300, 415):
+        for j in range(563, 650):
+            if key[i, j] < 100:
+                pyautogui.keyDown("up")
+                return
+
+    return
+
+
+if __name__ == '__main__':
+    print("Hey... Dino game about to start in 3 seconds...")
+    time.sleep(3)
+    # pyautogui.keyDown("up")
+    while True:
+        image = ImageGrab.grab().convert('L')
+        data = image.load()
+        jump(data)
